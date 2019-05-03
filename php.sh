2ef0ec4eb535c64ba26ba01ac60139d728c92fd5
raby87 <raby87@users.noreply.github.com>
2019-05-03 21:00:57 +0800

    Update php.sh

--- php.sh
+++ php.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
+varsion=7.2.4
 
+wget -c http://cn2.php.net/distributions/php-${varsion}.tar.gz
+
+tar -xzvf php-${varsion}.tar.gz
+
+yum install -y 
+libxml2*
+openssl*
+libcurl*
+libjpeg*
+libpng*
+freetype*
+libmcrypt*
+
+cd php-${varsion}
+
+./configure --prefix=/mnt/php7 --with-mysqli --with-pdo-mysql --with-iconv-dir --with-freetype-dir --with-jpeg-dir --with-png-dir --with-zlib --with-libxml-dir --enable-simplexml --enable-xml --disable-rpath --enable-bcmath --enable-soap --enable-zip --with-curl --enable-fpm --with-fpm-user=www --with-fpm-group=www --enable-mbstring --enable-sockets --with-gd --with-openssl --with-mhash --enable-opcache --disable-fileinfo
+
+make && make install
