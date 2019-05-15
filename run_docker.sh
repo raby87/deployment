dc0538de76cbea57bdb178cd44844e9c3df624c9
raby87 <raby87@users.noreply.github.com>
2019-05-15 17:55:36 +0800

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -8,3 +8,6 @@ docker build -t api .
 docker run --name c_api -d -p 8082:81 -v ${product_dir}:${product_dir} /bin/bash
 
 docker exec -it c_api /bin/sh
+
+#在主机上执行docker容器里面的脚本命令：docker exec -i 容器ID 命令
+#如：docker exec -i c_api bash ldapbakup.sh
