04f4de1437a1ffbf0a2fcbec72831b706f4512d2
raby87 <raby87@users.noreply.github.com>
2019-05-27 16:19:33 +0800

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -5,7 +5,7 @@ docker stop c_api
 docker rm c_api
 docker build -t api .
 
-docker run --name c_api -d -p 8082:81 -v ${product_dir}:${product_dir} /bin/bash
+docker run --name c_api -p 8082:81 -v ${product_dir}:${product_dir}  -d api 
 
 docker exec -it c_api /bin/sh
 
