fba5eca4466e5a03d63af1f86e309fad638f9ef9
raby87 <raby87@users.noreply.github.com>
2019-05-28 16:16:13 +0800

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -5,7 +5,8 @@ docker stop c_$product
 docker rm c_$product
 docker build -t $product .
 
-docker run --name c_$product -p 8082:81 -v /www/${product_dir}:/www  -d $product  --privileged=true
+docker run --name c_$product -p 8082:80 -v /www/${product_dir}:/www  -d $product  --privileged=true
+#docker run --name c_api -p 8082:80 -v /www/${product_dir}:/www -d api /bin/sh -c "sleep 1000000000"
 
 docker exec -it c_$product /bin/sh
 
