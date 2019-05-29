eb8bedd1d8e772df952b42253f08aedf7a06a26e
raby87 <raby87@users.noreply.github.com>
2019-05-29 17:36:16 +0800

    Update start.sh

--- k8s/start.sh
+++ k8s/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+rm -rf /var/lib/docker/*
+
 systemctl start etcd
 systemctl start docker
 systemctl start kube-apiserver
@@ -6,4 +8,7 @@ systemctl start kube-controller-manager
 systemctl start kube-scheduler
 systemctl start kubelet
 systemctl start kube-proxy
+
+
+docker pull registry.access.redhat.com/rhel7/pod-infrastructure:latest
 
